5e8ec4c146f0d11f3596383221f01a96ce59f426
Kwizera Pacifique <kwizerapacifique19@gmail.com>
2024-05-10 17:45:20 +0200

    mqtt messaging

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import pandas as pd
 import joblib
+import paho.mqtt.client as mqtt
+import json
+
 
 app = FastAPI()
 
@@ -12,6 +15,38 @@ hourMin = 8 # 8:00
 hourMax = 20 # 22:00
 
 model_timewise = joblib.load('./data/pool_model02_.pkl') # contains days & hours
+
+# MQTT setup
+mqtt_broker = "192.168.43.118"
+mqtt_port = 1883
+mqtt_topic_sensor = "pool/sensor"  # MQTT topic where sensor publishes data
+
+ph_value = 0
+tds_value = 0
+tbdt_value = 0
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    client.subscribe(mqtt_topic_sensor)
+    
+
+def on_message(client, userdata, msg):
+    global ph_value, tds_value, tbdt_value 
+    
+    # parse json data from mqtt
+    data = json.loads(msg.payload.decode())
+    
+    # Extract sensor data
+    tds_value = data.get("tds", 0)
+    ph_value = data.get("ph", 0)
+    tbdt_value = data.get("tbdt", 0)
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect(mqtt_broker, mqtt_port, 60)
+client.loop_start()  # Start MQTT loop
 
 # endpoint to determine parameters and safety using a specific day and hour
 @app.get("/parameters/predict/{day}/{hour}")
@@ -72,6 +107,13 @@ async def predict(today:int,hour_now:int):
     forecast = { 'day': abs(dif_day), 'hour' : abs(dif_hour) }
     
     return forecast
+
+# endpoint to provide real time data
+@app.get("/parameters/realTime")
+async def fetch():
+    liveData = { 'ph' : ph_value, 'tds' : tds_value, 'turbidity' : tbdt_value}
+    
+    return liveData
             
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)         
